54e4c8e85cfb0f1965b0ff3bb7cf82b99fad29f7
HadiElKarhani <102424840+HadiElKarhani@users.noreply.github.com>
2022-03-26 17:12:24 +0200

    Add files via upload

---
+++ Configuration.py
@@ -0,0 +1,22 @@
+# Extracting info from the txt file
+
+File_in = open('Configuration.txt', 'r')
+Lines = File_in.read().split('\n')
+
+Index = []
+for line in Lines:
+    Index.append(line.split('='))
+
+Dictionnary = {}
+
+for index in Index:
+    Dictionnary[index[0]]=index[1]
+
+def FExtractFile(): # Name of the output CSV file with extracted features
+    return str(Dictionnary['FeaturesFile'])
+
+def ClassificationsFile(): # Name of the output CSV file with classified domains
+    return str(Dictionnary['ClassificationsFile'])
+
+def Requests(): # Number of requests per run
+    return int(Dictionnary['Requests'])
---
+++ model.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@authors: Hadi El Karhani, Yorgo Bou Samra, Riad Jamal
+"""
+import pandas as pd
+import numpy as np
+import sklearn # pip3 install scikit-learn==0.23.2
+from sklearn import svm , preprocessing
+import pickle
+
+class Detect:
+    def __init__(self, datafile ,weights = 'finalized_model.sav'):
+        self.df = pd.read_csv(datafile)
+        self.df = self.df.fillna(self.df.median()).clip(-1e11,1e11)
+        self.X = self.df.values
+        self.loaded_model = pickle.load(open(weights, 'rb'))
+
+    def predict(self):
+        result = self.loaded_model.predict(self.X)
+        list_result = result.tolist()
+        for index in range(len(list_result)):
+            if list_result[index] == -1:
+                list_result[index] = "Phishing attack"
+            elif list_result[index] == 1:
+                list_result[index] = "Safe"
+            else:
+                pass
+
+        return list_result
+
+#if __name__ == '__main__':
+#    filename = input("Please Enter File Path Here: ")
+#    model_instance = Detect(filename)
+#    print(model_instance.predict())
