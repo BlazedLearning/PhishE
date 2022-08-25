e6ae38868db94c12b40974322f67ce2c80aa024e
HadiElKarhani <102424840+HadiElKarhani@users.noreply.github.com>
2022-08-25 16:47:06 +0300

    Update keys.py

--- keys.py
+++ keys.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 import Configuration
 
-misp_url = Configuration.MISP_URL()
+misp_url = Configuration.MISP_URL() # The MISP instance URL
 misp_key = Configuration.MISP_Key() # The MISP auth key can be found on the MISP web interface under the automation section
 misp_verifycert = False
 misp_client_cert = ''
